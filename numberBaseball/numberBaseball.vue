2311f555f8c35cd77063bedc246a899a2d7ca99b
ChoiWonGyun (Ewan) <chl8263@naver.com>
2019-12-31 15:59:12 -0800

    vue installer

---
+++ numberBaseball/numberBaseball.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1> {{result}} </h1>
+        <form v-on:submit="onSubmitForm()">
+            <input type="number" ref="answer" maxlength="4" v-model="value"/>
+            <button>Enter</button>
+        </form>
+        <div>count : {{}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                value: '',
+                resurt: ''
+            }
+        },
+        methods:{
+            onSubmitForm(e){
+                e.preventDefault();
+            }
+        }
+    };
+</script>
+
+<style>
+
+</style>
