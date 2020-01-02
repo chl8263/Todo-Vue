aa6457a886556512c10baaf324756156d208fe2a
Ewan <chl8263@naver.com>
2020-01-01 23:43:11 -0800

    rockScissorsPaper

---
+++ rockScissorsPaper/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';  //get pakage from pakage.json
+import rockScissorsPaper from './rockScissorsPaper';
+
+new Vue(rockScissorsPaper).$mount("#root");
---
+++ rockScissorsPaper/rockScissorsPaper.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <div id="computer" :style="computedStyleObject"></div>
+        <div>
+            <button @click="onclickButton('rock')">바위</button>
+            <button @click="onclickButton('scissors')">가위</button>
+            <button @click="onclickButton('paper')">보</button>
+        </div>
+        <div>{{result}}</div>
+        <div>Present score {{score || 0}}</div>
+    </div>
+</template>
+ 
+<script>
+    const rspCoords = {
+        rock: '0px',
+        scissors: -'-142px',
+        paper: '-284px'
+    };
+    export default {
+        data(){
+           return {
+               imgcode: '',
+               result: '',
+               score: '',
+               imgcode: ''
+           };
+        },
+        computed: {
+            computedStyleObject(){
+                return {
+                    background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgcode} 0` 
+                }
+            }
+           
+        },
+        methods:{
+          onclickButton(choice){
+
+          }
+        }
+    };
+</script>
+
+<style scoped>
+    #screen {
+        width: 300px;
+        height: 200px;
+        text-align: center;
+        user-select: none;
+    }
+    #screen.waiting {
+        background-color: aqua;
+    }
+    #screen.ready {
+        background-color: red;
+    }
+    #screen.now {
+        background-color: greenyellow;
+    }
+</style>
